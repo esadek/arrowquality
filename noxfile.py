9773e276ca62ee21fce8eeacd099a6af78343388
Emil Sadek <esadek@users.noreply.github.com>
2025-03-19 00:13:44 -0700

    Change: reuse venv

--- noxfile.py
+++ noxfile.py
@@ -5,21 +5,22 @@ MAX_PYTHON_VERSION = "3.13"
 PYTHON_VERSIONS = nox.project.python_versions(PYPROJECT, max_version=MAX_PYTHON_VERSION)
 
 nox.options.default_venv_backend = "uv"
+nox.options.reuse_venv = "yes"
 
 
-@nox.session(python=PYTHON_VERSIONS, reuse_venv=True)
+@nox.session(python=PYTHON_VERSIONS)
 def tests(session: nox.Session) -> None:
     session.install(".", "pytest")
     session.run("pytest")
 
 
-@nox.session(python=MAX_PYTHON_VERSION, reuse_venv=True)
+@nox.session(python=MAX_PYTHON_VERSION)
 def lint(session: nox.Session) -> None:
     session.install("ruff")
     session.run("ruff", "check", "--fix")
 
 
-@nox.session(python=MAX_PYTHON_VERSION, reuse_venv=True)
+@nox.session(python=MAX_PYTHON_VERSION)
 def types(session: nox.Session) -> None:
     session.install("mypy")
     session.run("mypy", "src/arrowquality", "tests", "--strict")
