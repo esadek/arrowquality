bfbfd12704686b579099c7bd3fed295f40719c53
Emil Sadek <esadek@users.noreply.github.com>
2025-03-19 00:29:43 -0700

    Remove: sessions

--- noxfile.py
+++ noxfile.py
@@ -1,8 +1,7 @@
 import nox
 
 PYPROJECT = nox.project.load_toml("pyproject.toml")
-MAX_PYTHON_VERSION = "3.13"
-PYTHON_VERSIONS = nox.project.python_versions(PYPROJECT, max_version=MAX_PYTHON_VERSION)
+PYTHON_VERSIONS = nox.project.python_versions(PYPROJECT, max_version="3.13")
 
 nox.options.default_venv_backend = "uv"
 nox.options.reuse_venv = "yes"
@@ -12,15 +11,3 @@ nox.options.reuse_venv = "yes"
 def tests(session: nox.Session) -> None:
     session.install(".", "pytest")
     session.run("pytest")
-
-
-@nox.session(python=MAX_PYTHON_VERSION)
-def lint(session: nox.Session) -> None:
-    session.install("ruff")
-    session.run("ruff", "check", "--fix")
-
-
-@nox.session(python=MAX_PYTHON_VERSION)
-def types(session: nox.Session) -> None:
-    session.install("mypy")
-    session.run("mypy", "src/arrowquality", "tests", "--strict")
